64713805b0ad5b5bdf15099ef51c5912aa52aa97
Martynas Beinoras <martynas.beinoras@gmail.com>
2015-10-16 12:43:12 +0300

    Initial commit

---
+++ dev/data/chart_data.js
@@ -0,0 +1,179 @@
+var chart_data = {
+	set_labels:
+		[
+			{
+				value: "2014"
+			},
+			{
+				value: "2015"
+			}
+		],
+	set:
+		[
+			{
+				label: "Vilnius",
+				coord: {
+					lat: 54.686946,
+					lng: 25.280693
+				},
+				subset: [
+					{
+						value: 529022
+					},
+					{
+						value: 531947
+					}
+				]
+			},
+			{
+				label: "Kaunas",
+				coord: {
+					lat: 54.898606,
+					lng: 23.900369
+				},
+				subset: [
+					{
+						value: 304012
+					},
+					{
+						value: 301296
+					}
+				]
+			},
+			{
+				label: "Klaipėda",
+				coord: {
+					lat: 55.703905,
+					lng: 21.132993
+				},
+				subset: [
+					{
+						value: 157305
+					},
+					{
+						value: 156122
+					}
+				]
+			},
+			{
+				label: "Šiauliai",
+				coord: {
+					lat: 55.935320,
+					lng: 23.313055
+				},
+				subset: [
+					{
+						value: 105610
+					},
+					{
+						value: 104619
+					}
+				]
+			},
+			{
+				label: "Panevėžys",
+				coord: {
+					lat: 55.734488,
+					lng: 24.362919
+				},
+				subset: [
+					{
+						value: 96328
+					},
+					{
+						value: 95218
+					}
+				]
+			}
+		]
+};
+
+var chart_data_us = {
+	set_labels:
+		[
+			{
+				value: "2010"
+			},
+			{
+				value: "2014"
+			}
+		],
+	set:
+		[
+			{
+				label: "New York",
+				coord: {
+					lat: 40.6643,
+					lng: -73.9385
+				},
+				subset: [
+					{
+						value: 8175133
+					},
+					{
+						value: 8491079
+					}
+				]
+			},
+			{
+				label: "Los Angeles",
+				coord: {
+					lat: 34.0194,
+					lng: -118.4108
+				},
+				subset: [
+					{
+						value: 3792621
+					},
+					{
+						value: 3928864
+					}
+				]
+			},
+			{
+				label: "Chicago",
+				coord: {
+					lat: 41.8376,
+					lng: -87.6818
+				},
+				subset: [
+					{
+						value: 2695598
+					},
+					{
+						value: 2722389
+					}
+				]
+			},
+			{
+				label: "Houston",
+				coord: {
+					lat: 29.7805,
+					lng: -95.3863
+				},
+				subset: [
+					{
+						value: 2100263
+					},
+					{
+						value: 2239558
+					}
+				]
+			},
+			{
+				label: "Philadelphia",
+				coord: {
+					lat: 40.0094,
+					lng: -75.1333
+				},
+				subset: [
+					{
+						value: 1526006
+					},
+					{
+						value: 1560297
+					}
+				]
+			}
+		]
+};
